25f67e9b649347262b33237564481e9ecd5fbf62
Kevin Oberlag <koberlag@gmail.com>
2016-01-21 21:26:57 -0600

    setup calendar functionality in app. Next step is to refactor into a directive.

--- app/app.js
+++ app/app.js
@@ -1,3 +1,25 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
+.controller('calendarController', [ '$scope', function($scope){
+	$scope.currentYear = new Date().getFullYear();
+	$scope.currentMonth = new Date().getMonth();
+	$scope.selectedYear = $scope.currentYear;
+	$scope.selectedMonth = $scope.currentMonth;
+	$scope.setCalendar = function(){
+		var date = new Date($scope.selectedYear, $scope.selectedMonth);
+		var range = CalendarRange.getMonthlyRange(date);
+		$scope.weeks = range.days.length / 7;
+		$scope.days = range.days;
+	};
+	$scope.setCalendar();
+	$scope.years = [];
+	
+	var startingYear = $scope.currentYear - 20,
+		endingYear = $scope.currentYear + 20;
+
+	for (var i = startingYear; i <= endingYear; i++) {
+		$scope.years.push(i);
+	}
+
+}]);
