1f69628195a0c3beef6ef5160c864a2f1c541733
Kevin Oberlag <koberlag@gmail.com>
2016-01-23 16:40:34 -0600

    moved calendar into directive

--- app/app.js
+++ app/app.js
@@ -2,24 +2,33 @@ angular.module('calendarDemoApp', [])
 
 // your controller and directive code go here
 .controller('calendarController', [ '$scope', function($scope){
-	$scope.currentYear = new Date().getFullYear();
-	$scope.currentMonth = new Date().getMonth();
-	$scope.selectedYear = $scope.currentYear;
-	$scope.selectedMonth = $scope.currentMonth;
-	$scope.setCalendar = function(){
-		var date = new Date($scope.selectedYear, $scope.selectedMonth);
-		var range = CalendarRange.getMonthlyRange(date);
-		$scope.weeks = range.days.length / 7;
-		$scope.days = range.days;
-	};
-	$scope.setCalendar();
-	$scope.years = [];
 	
-	var startingYear = $scope.currentYear - 20,
-		endingYear = $scope.currentYear + 20;
 
-	for (var i = startingYear; i <= endingYear; i++) {
-		$scope.years.push(i);
-	}
+}]).directive('calendar', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'calendar-template.html',
+        scope: true,
+        link: function(scope, element, attrs) {
+            scope.currentYear = new Date().getFullYear();
+			scope.currentMonth = new Date().getMonth();
+			scope.selectedYear = scope.currentYear;
+			scope.selectedMonth = scope.currentMonth;
+			scope.setCalendar = function(){
+				var date = new Date(scope.selectedYear, scope.selectedMonth);
+				var range = CalendarRange.getMonthlyRange(date);
+				scope.weeks = range.days.length / 7;
+				scope.days = range.days;
+			};
+			scope.setCalendar();
+			scope.years = [];
 
-}]);
+			var startingYear = scope.currentYear - 20,
+				endingYear = scope.currentYear + 20;
+
+			for (var i = startingYear; i <= endingYear; i++) {
+				scope.years.push(i);
+			}
+        }
+    };
+});
